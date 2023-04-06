484812170fbd04cf3a43bffb21fe64e375581099
My Duyen <duyenb1910046@student.ctu.edu.vn>
2023-04-06 10:50:12 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  export default {
+
+  }
+</script>
+
+<template>
+  <h1>Hello, Vue.js!</h1>
+</template>
+
+<style>
+  .page {
+    max-width: auto;
+    margin: auto;
+  }
+</style>
